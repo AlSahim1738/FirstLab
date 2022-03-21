f55817e710afd204c1b630942ea40ddc413a4c2e
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-03-21 19:54:34 +0100

    Create ComputeReal.java

---
+++ ComputeReal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrateur
+ */
+public class Compute 
+{
+    static int computeThat(int A, int B)
+    {
+        int R;
+        R = (int) (-1-(A+B)/0.2*(B-A));
+        return R;
+    }
+}
