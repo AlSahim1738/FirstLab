e8d251ead6f550341805ee399dcef692e67e7404
AlSahim1738 <94036149+AlSahim1738@users.noreply.github.com>
2022-03-21 19:45:40 +0100

    Create ComputeNGTest.java

---
+++ ComputeNGTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class ComputeNGTest {
+    
+    public ComputeNGTest() {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+        
+    }
+
+    @Test
+    public void testSomeMethod() {
+        int a , b ;
+        a = 3 ;
+        b = 7;
+        assertEquals(Compute.computeThat(a, b) , a);
+        assertEquals(Compute.computeThat(a, b) , a+b);
+        assertEquals(Compute.computeThat(a, b) , a*b);
+    }
+    
+}
